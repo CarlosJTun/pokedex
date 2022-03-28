26937a86f05cf8fe69249a8a6d55b09638a87038
CarlosJTun <tecnotun@outlook.com>
2022-03-27 23:19:12 -0600

    Se leen de la API los datos de nombre, tipo, altura, peso y 3 movimientos y se muestran en la pokedex.

--- pokedex.js
+++ pokedex.js
@@ -13,10 +13,23 @@ const fetchPokemon = () => {
         }
     }).then((data) => {
         if (data) {
+            
             console.log(data);
+            
             let pokeImg = data.sprites.front_default;
             pokeImage(pokeImg);
-            console.log(pokeImg);
+            //console.log(pokeImg);
+
+            let dataName = convertir_tipo_titulo(data.name);
+            let dataType = convertir_tipo_titulo(data.types[0].type.name) //  Mejorar para permitir pokemones de 2 tipos, como "Rayquaza"  ... + " " + convertir_tipo_titulo(data.types[1].type.name);
+            let dataHeight = data.height/10; // metros     Mejorar para  convertir a pies con pulgadas
+            let dataWeight = data.weight/10; //kg    Mejorar para convertir a libras
+            let dataMove1 = convertir_tipo_titulo(data.moves[0].move.name);
+            let dataMove2 = convertir_tipo_titulo(data.moves[1].move.name);
+            let dataMove3 = convertir_tipo_titulo(data.moves[3].move.name);
+            console.log(dataName, dataType, dataHeight, dataWeight, dataMove1, dataMove2, dataMove3);
+            pokeData(dataName, dataType, dataHeight, dataWeight, dataMove1, dataMove2, dataMove3);
+
         }
     });
 }
@@ -26,6 +39,13 @@ const pokeImage = (url) => {
     pokePhoto.src = url;
 }
 
+const pokeData = (dataName, dataType, dataHeight, dataWeight, dataMove1, dataMove2, dataMove3) => {
+    document.getElementById("setName").innerHTML = "<strong>Name:</strong> " + dataName;
+    document.getElementById("setType").innerHTML = "<strong>Type:</strong> " + dataType;
+    document.getElementById("setHeight").innerHTML = "<strong>Height:</strong>  " + dataHeight + "m";
+    document.getElementById("setWeight").innerHTML = "<strong>Height:</strong>  " + dataWeight + "kg";
+    document.getElementById("setMoves").innerHTML = dataMove1 + ", " + dataMove2 + " & " + dataMove3 + ".";
+}
 
 
 // Get the input field
@@ -41,3 +61,8 @@ input.addEventListener("keyup", function(keyboardEvent) {
     document.getElementById("searchbtn").click();
   }
 });
+
+function convertir_tipo_titulo(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
